29eb412cde7fdc46a91018a1dcb7245012654626
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-12-15 18:39:01 +0100

    code added

---
+++ blog/src/components/pages/BlogList.test.tsx
@@ -0,0 +1,25 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import { useBlogs } from '../../../hooks/useBlogs'
+import { BlogCard } from '../../blog-card/BlogCard';
+import { Navigation } from '../../navigation/Navigation';
+
+export const BlogList = () => {
+const { blogsList } = useBlogs();
+
+  return (
+    <Container>
+      <Navigation blogsCount={blogsList.length}/>
+      <h1>My blog</h1>
+      <Container>
+        <Row>
+          {blogsList.length ? blogsList.map((blog, index) => (
+            <Col>
+              <BlogCard blog={blog} key={index} />
+            </Col>
+          )) : <p>There is no blogs</p>}
+          {}
+        </Row>
+      </Container>
+    </Container>
+  )
+}
