0a3f0585684f89302bb563be0c2215af1d9d9286
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-12-15 21:33:09 +0100

    code added

--- blog/src/components/pages/BlogList.test.tsx
+++ blog/src/components/pages/BlogList.test.tsx
@@ -1,24 +1,30 @@
 import { render, screen } from '@testing-library/react'
 import { BlogList } from "./BlogList"
 import { server } from '../mocks/server'
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+import { rest } from 'msw';
 
 describe('BlogList.tsx', () => {
-    test('should render two blogs', async() => {
-        render(<BlogList />)
+  test('should render two blogs', async () => {
+    render(<BlogList />);
+    const title = await screen.findAllByText(/test title/i);
+    expect(title.length).toBe(2);
+  });
 
-        const title = await screen.findAllByText(/test title/i)
-        expect(title.length).toBe(2)
-    })
+  test('should render no blogs message', async () => {
+    render(<BlogList />);
 
-    test('spinner is rendering', () => {
-		render(<BlogList />);
+    await server.use(
+      rest.get('http://localhost:3000/blogs', (req, res, ctx) => (
+        res(ctx.json([]))
+      ))
+    )
+    const message = await screen.findByText("There is no blogs");
+    expect(message).toBeInTheDocument();
+  });
 
-		const spinner = screen.getByTestId('spinner');
-        expect(spinner).toBeInTheDocument()
-    })
-
+  test('spinner is rendered', () => {
+    render(<BlogList />);
+    const spinner = screen.getByTestId('spinner');
+    expect(spinner).toBeInTheDocument()
+  })
 })
