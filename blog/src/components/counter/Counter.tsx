dd93638f0ece823261fcf151ca5d20f7a79d12c5
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-09 20:21:17 +0100

    code added

--- blog/src/components/counter/Counter.tsx
+++ blog/src/components/counter/Counter.tsx
@@ -2,25 +2,33 @@ import { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
 
 export const Counter = () => {
-	const [value, setValue] = useState(0);
+	const [value, setValue] = useState<number | string >(0);
+    const [initialValue, setInitialValue] = useState('')
 
 	const handlePlusClick = () => {
-		setValue(value + 1);
+        if (typeof value === 'number') setValue(value + 1);
+		
 	};
 
 	const handleMinusClick = () => {
-		setValue(value - 1);
+		if (typeof value === 'number') setValue(value - 1);
 	};
 
 	const handleResetClick = () => {
 		setValue(0);
 	};
 
+    const handleValueChange = (value: string) => {
+        console.log(parseInt(value))
+    }
+
 	return (
 		<Container>
 			<h2>Counter</h2>
+            <span>Initial value</span>
+            <input value={initialValue} onChange={event => handleValueChange(event.target.value)}/>
 			<Button onClick={handleMinusClick}>-</Button>
-			<span data-testid="counterValue">{value}</span>
+			<span data-testid="counterValue">{initialValue? initialValue : value}</span>
 			<Button onClick={handlePlusClick}>+</Button>
 			<Container>
 				<Button onClick={handleResetClick}>Reset</Button>
