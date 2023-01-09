8ed935371b62b90d0a548c8a3fb6b2003ece0983
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-09 20:45:51 +0100

    modified

--- blog/src/components/counter/Counter.tsx
+++ blog/src/components/counter/Counter.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
 
 export const Counter = () => {
-	const [value, setValue] = useState<number | string >(0);
-    const [initialValue, setInitialValue] = useState('')
+	const [value, setValue] = useState<number | string>(0);
+	const [initialValue, setInitialValue] = useState('');
+
+	useEffect(() => {
+		if (initialValue) setValue(initialValue);
+	}, [initialValue]);
 
 	const handlePlusClick = () => {
-        if (typeof value === 'number') setValue(value + 1);
-		
+		setValue(+value + 1);
 	};
 
 	const handleMinusClick = () => {
-		if (typeof value === 'number') setValue(value - 1);
+		setValue(+value - 1);
 	};
 
 	const handleResetClick = () => {
 		setValue(0);
 	};
 
-    const handleValueChange = (value: string) => {
-        console.log(parseInt(value))
-    }
-
 	return (
 		<Container>
 			<h2>Counter</h2>
-            <span>Initial value</span>
-            <input value={initialValue} onChange={event => handleValueChange(event.target.value)}/>
-			<Button onClick={handleMinusClick}>-</Button>
-			<span data-testid="counterValue">{initialValue? initialValue : value}</span>
-			<Button onClick={handlePlusClick}>+</Button>
+			<span>Initial value:</span>
+			<input
+				value={initialValue}
+				onChange={(event) => setInitialValue(event.target.value)}
+			/>
+			<Container>
+				<Button onClick={handleMinusClick}>-</Button>
+				<span data-testid="counterValue">{value}</span>
+				<Button onClick={handlePlusClick}>+</Button>
+			</Container>
 			<Container>
 				<Button onClick={handleResetClick}>Reset</Button>
 			</Container>
