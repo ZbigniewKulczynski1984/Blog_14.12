ca5df2ebae035b760f0e0872094016651fb4ab43
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-09 18:57:46 +0100

    code added

--- blog/src/components/counter/Counter.tsx
+++ blog/src/components/counter/Counter.tsx
@@ -1,16 +1,30 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
 
 export const Counter = () => {
-    const [value, setValue] = useState(0)
+	const [value, setValue] = useState(0);
 
+	const handlePlusClick = () => {
+		setValue(value + 1);
+	};
+
+	const handleMinusClick = () => {
+		setValue(value - 1);
+	};
+
+	const handleResetClick = () => {
+		setValue(0);
+	};
 
 	return (
 		<Container>
 			<h2>Counter</h2>
-			<Button>-</Button>
+			<Button onClick={handleMinusClick}>-</Button>
 			<span>{value}</span>
-			<Button>+</Button>
+			<Button onClick={handlePlusClick}>+</Button>
+			<Container>
+				<Button onClick={handleResetClick}>Reset</Button>
+			</Container>
 		</Container>
 	);
 };
